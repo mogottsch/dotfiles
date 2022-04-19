1d7863014d98da2246e48cb3fa91dfcd03b233e0
Moritz <mgottsc5@smail.uni-koeln.de>
2022-04-19 09:10:38 +0200

    Configure rest client variables

---
+++ nvim/.config/nvim/lua/mogottsch/rest.lua
@@ -0,0 +1,9 @@
+require("rest-nvim").setup({
+    custom_dynamic_variables = {
+        url = "https://dev-api.meldescheine.de/api",
+        username = "DemoV",
+        token = "",
+        objekt = "2049",
+        gemeinde = "1"
+    }
+})
--- nvim/.config/nvim/plugin/lua.vim
+++ nvim/.config/nvim/plugin/lua.vim
@@ -3,5 +3,6 @@ lua require('mogottsch.lsp')
 lua require('mogottsch.completion')
 lua require('mogottsch.git-worktree')
 lua require('mogottsch.refactor')
+lua require('mogottsch.rest')
 
 lua require('Comment').setup()

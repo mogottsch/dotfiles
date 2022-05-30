912147476b9919302d6d7de548f9ed97da3055ed
Moritz <moritz@zendri.com>
2022-05-30 09:13:23 +0200

    Add required lsp servers

--- nvim/.config/nvim/lua/mogottsch/lsp.lua
+++ nvim/.config/nvim/lua/mogottsch/lsp.lua
@@ -2,17 +2,27 @@ local lsp_installer = require("nvim-lsp-installer")
 
 local custom_attach = function() print("LSP started."); end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
+local servers = {
+    "ansiblels", "bashls", "dockerls", "golangci_lint_ls", --[[ "grammarly", ]]
+    "graphql", "jsonls", "pyright", "yamlls", "cssls", "emmet_ls", "eslint",
+    "gopls", "html", "intelephense", "prismals", "r_language_server",
+    "sumneko_lua", "tsserver", "vimls"
+}
+
+lsp_installer.setup({ensure_installed = servers})
+
+local lspconfig = require("lspconfig")
+
+local function on_attach(client, bufnr)
     local opts = {}
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
     --     opts.root_dir = function() ... end
     -- end
+    custom_attach()
 
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup{on_attach = custom_attach}
-end)
+    -- server:setup{on_attach = custom_attach}
+end
+
+for _, lsp in pairs(servers) do lspconfig[lsp].setup {on_attach = on_attach} end
